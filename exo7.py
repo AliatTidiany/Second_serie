26574a6c2a4ccf1885b2df638fb15b790a5e3f43
Alioune Mbodji <ambodj92@gmail.com>
2022-05-17 23:34:35 +0000

    2iéme et 3iéme methode: avec for et while

--- exo7.py
+++ exo7.py
@@ -1,9 +1,21 @@
-##
+##Premiere methode
 
 
 def som(i,j):
     n=(j-i)+1
     s=(n*(j+i))/2
-    return s 
-t=som(2,4)
-print(t)
+##Deuxiéme méthode while
+    s1=0
+    y=i
+    while (y<j+1):
+        s1=s1+y
+        y=y+1
+##Troisiéme méthode
+    s2=0
+    for x in range (i,j+1):
+        s2=s2+x
+    return s, s1, s2
+Result=som(2,4)
+print(Result)
+
+
